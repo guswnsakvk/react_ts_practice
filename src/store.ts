512d04f7e4b3a082df8b4d41aabf45f936f57162
guswnsakvk <guswnsakvk1@naver.com>
2023-06-22 16:37:57 +0900

    resturant type을 가진 data 만들기

---
+++ src/store.ts
@@ -0,0 +1,56 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { Resturant } from './model/resturant'
+
+let data:Resturant = {
+  name:'맛있는 식당',
+  category:'e',
+  address:{
+    city:'busan',
+    detail:'somewhere',
+    zipCode:2345
+  },
+  menu:[
+    {
+      name:'rose pasta',
+      price:2000,
+      category:'pasta'
+    },
+    {
+      name:'garlic steak',
+      price:3000,
+      category:'steak'
+    }
+  ]
+}
+
+let ResturantData = createSlice({
+  name: 'ResturantData',
+  initialState: data,
+  reducers: {
+
+  }
+})
+
+let test = createSlice({
+  name : 'test',
+  initialState : [1,2,3],
+  reducers : {
+
+  }
+})
+
+export const store = configureStore({
+  reducer: {
+    ResturantData : ResturantData.reducer,
+    test : test.reducer
+  }
+})
+
+export type RootState = ReturnType<typeof store.getState>
+
+//export default configureStore({
+//  reducer: { 
+//    ResturantData : ResturantData.reducer,
+//    //test : test.reducer
+//  }
+//})
